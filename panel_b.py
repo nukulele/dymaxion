1e297a6f42c9c8749837e4093f41bd0f85d28f04
David Gehrig <david@zemblan.org>
2013-06-11 21:25:59 -0500

    final panel_a, but busted panel_b

--- panel_b.py
+++ panel_b.py
@@ -33,7 +33,7 @@ def make_map( filename ):
 
 
     # set up our triangle
-    c = get_letter_canvas( filename )
+    c = get_letter_canvas( filename, clip=False )
 
     # hex grid
     c.setStrokeColor( (0.75, 0.75, 1) )
@@ -48,10 +48,10 @@ def make_map( filename ):
     d20.make_faces()
     mappings = face_mappings( d20 )
 
-    face_plane = d20.faces[0].plane
-    rot_mat = mappings.face_dict['a'][0]
+    face_plane = d20.faces[5].plane
+    rot_mat = mappings.face_dict['f'][0]
  
-    for lat in range( 0, 40, 1 ):
+    for lat in range( 10, 170, 1 ):
         if lat % 5:
             c.setLineWidth( 0.0005 )
         else:
@@ -59,50 +59,25 @@ def make_map( filename ):
 
         lat_rad = to_rad( lat ).evalf()
         try:
-            point = line_plane_intersect( two_point_line( _sphere_to_cart( lat_rad, 0 ), zero_3d ), face_plane )
+            point = line_plane_intersect( two_point_line( zero_3d, _sphere_to_cart( lat_rad, 5*_pi/6 ) ), face_plane )
             mp1 = point.multiply( rot_mat )
-            point = line_plane_intersect( two_point_line( _sphere_to_cart( lat_rad, _pi/2 ), zero_3d ), face_plane )
+            point = line_plane_intersect( two_point_line( zero_3d, _sphere_to_cart( lat_rad, _pi ) ), face_plane )
             mp2 = point.multiply( rot_mat )
-            point = line_plane_intersect( two_point_line( _sphere_to_cart( lat_rad, -_pi/2 ), zero_3d ), face_plane )
+            point = line_plane_intersect( two_point_line( zero_3d, _sphere_to_cart( lat_rad, 7*_pi/6 ) ), face_plane )
             mp3 = point.multiply( rot_mat )
             
             the_circle = three_point_circle( mp1[0], mp1[1], mp2[0], mp2[1], mp3[0], mp3[1] )
+    
+            
             c.circle( the_circle.x0, the_circle.y0, the_circle.radius, fill=0, stroke=1 )
             
         except:
             pass
   
-    for lon in range( 0, 360, 1 ):
-    
-        if lon % 5:
-            c.setLineWidth( 0.0005 )
-        else:
-            c.setLineWidth( 0.001 )
-        
-        try:
-            point = line_plane_intersect( two_point_line( _sphere_to_cart( _pi/4, to_rad( lon).evalf() ), zero_3d ), face_plane )
-            mp1 = point.multiply( rot_mat )
-            
-            end_pt = _pi/9
-            if (lon % 5) == 0:
-                end_pt = _pi/18
-            if (lon % 10) == 0:
-                end_pt = _pi/36
-            if (lon % 30) == 0:
-                end_pt = 0
-            
-            point = line_plane_intersect( two_point_line( _sphere_to_cart( end_pt, to_rad( lon).evalf() ), zero_3d ), face_plane )
-            mp2 = point.multiply( rot_mat )
-            c.line( mp1[0], mp1[1], mp2[0], mp2[1] )
-            
-        except:
-            pass
-            
-        
 
 
     c.showPage()
     c.save()
     
 if __name__ == '__main__':
-    make_map( "panel_a.pdf" )        
+    make_map( "panel_b.pdf" )        
